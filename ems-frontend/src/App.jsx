5e3d8ef875c00974f5d1347549a0c497d762eddc
Ignacio <casaresignacio17@gmail.com>
2025-02-24 17:58:53 -0300

    Adding List Employee commponent

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <></>;
+	return (
+		<>
+			<ListEmployeeComponent />
+		</>
+	);
 }
 
 export default App;
